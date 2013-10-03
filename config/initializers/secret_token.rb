76808d38afeda225b5621882211fe7ad6c8932d9
Attila Csala <attilacsala@gmail.com>
2013-10-03 22:34:45 +0200

    User model added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LogIn::Application.config.secret_key_base = 'd5dec212a33d0c8b03dd7610acd34c9d625cc4d1f84bcf88d91e9994da6a4cc72857e65dccc2a109a6d9e3fcddc3ed53bf3edccf48a6779524b5829a691d581f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LogIn::Application.config.session_store :cookie_store, key: '_LogIn_session'
